8666a5b98952d2c791ac30d745fd0f2024cf6eb4
Taylor Wilson <taylorjwilson1815@gmail.com>
2023-01-29 19:00:35 -0500

    Finished Calculator with operations included

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 let num1 = 0, num2 = 0;
 let operator_flag = 0;
+let ent_flag = 0;
 let live_flag = 0;
 
 const numbtn = document.querySelectorAll('button.number');
 const display = document.querySelector('.display');
 const clrbtn = document.querySelector('#clear');
 const operbtn = document.querySelectorAll('.operation');
-
+const entbtn = document.querySelector('#enter');
 
 //Number Button function
 numbtn.forEach((button) => {
@@ -14,13 +15,17 @@ numbtn.forEach((button) => {
         
         //First number entry
         if(operator_flag == 0){
+            if(ent_flag == 1){
+                num1 = 0;
+                ent_flag = 0;
+            }
             num1 = (10 * num1) + parseInt(button.id);
             display.textContent = num1
 
         }
 
         //Second number entry
-        if(operator_flag == 1){
+        if(operator_flag != 0){
             num2 = (10 * num2) + parseInt(button.id);
             display.textContent = num2;
 
@@ -45,4 +50,33 @@ operbtn.forEach((button) => {
     })
 })
 
+
+//Enter button function
+entbtn.addEventListener('click', () => {
+    ent_flag = 1;
+    if(operator_flag == '+'){
+        operator_flag = 0;
+        num1 = num1 + num2;
+        num2 = 0;
+        display.textContent = num1;
+    }
+    if(operator_flag == '-'){
+        operator_flag = 0;
+        num1 = num1 - num2;
+        num2 = 0;
+        display.textContent = num1;
+    }
+    if(operator_flag == '*'){
+        operator_flag = 0;
+        num1 = num1 * num2;
+        num2 = 0;
+        display.textContent = num1;
+    }
+    if(operator_flag == '/'){
+        operator_flag = 0;
+        num1 = num1 / num2;
+        num2 = 0;
+        display.textContent = num1;
+    }
+})
 
